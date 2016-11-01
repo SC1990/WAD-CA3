7ec23d6d661eb0ff682579ca9848a9ed1bb1e837
stuartcarroll91 <stuartcarroll91@gmail.com>
2016-11-01 16:09:42 -0400

    1/11/16

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 echo "All Contacts <br> <br>";
 
+//load xml file
 $xml=simplexml_load_file("contacts.xml") or die("Error: Cannot create object");
 
+//print each child element of contacts
 foreach($xml->children() as $contacts) { 
   echo $contacts->name . "<br>";
   echo $contacts->address->street . ", ";
