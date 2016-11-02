f3749d236af9d27701a14917310e626889ff009c
stuartcarroll91 <stuartcarroll91@gmail.com>
2016-11-02 17:28:21 -0400

    2/11/16

--- addNewContact.php
+++ addNewContact.php
@@ -1,2 +1,68 @@
-Contact Name: <?php echo $_POST["name"]; ?><br>
-Email address: <?php echo $_POST["email"]; ?>
+<?php 
+
+
+if(isset($_POST['insert'])){
+  $xml = new DomDocument("1.0","UTF-8");
+  $xml->load('contacts.xml');
+  
+  $name = $_POST['name'];
+ /* $email = $_POST['email'];
+  $phoneNum = $_POST['phoneNum'];
+  $company = $_POST['company'];
+  $companyRole = $_POST['companyRole'];*/
+  
+  $rootTag = $xml->getElementsByTagName("contacts")->item(0);
+  
+  $contactTag = $xml->createElement("contact");
+  
+  $nameTag = $xml->createElement("name", $name);
+ /* $emailTag = $xml->createElement("email", $email);
+  $phoneNumTag = $xml->createElement("phoneNum", $phoneNum);
+  $companyTag = $xml->createElement("company", $company);
+  $companyRoleTag = $xml->createElement("companyRole", $companyRole);*/
+  
+  $contactTag->appendChild($nameTag);
+  /*$contactTag->appendChild($emailTag);
+  $contactTag->appendChild($phoneNumTag);
+  $contactTag->appendChild($companyTag);
+  $contactTag->appendChild($companyRoleTag);*/
+  
+  $rootTag->appendChild($contactTag);
+  
+  $xml->save('contacts.xml');
+}
+
+?>
+
+<html>
+<body>
+
+<!-- Add new contact-->
+  <h3 id="addNew">Add New Contact</h3>
+  
+  
+  
+  <!--contact form-->
+  <form action="addNewContact.php" method="POST" >
+    Name:<br>
+    <input type="text" name="name">
+    <br>
+    <!--
+    Email:<br>
+    <input type="text" name="email">
+    <br>
+    Phone Number:<br>
+    <input type="text" name="phoneNum">
+    <br>
+    Company:<br>
+    <input type="text" name="company">
+    <br>
+    Company Role:<br>
+    <input type="text" name="companyRole"> -->
+    <br>
+    <br>
+    <input type="submit" name="insert" value="Add">
+  </form>
+  
+  </body>
+  </html>
--- viewContacts.php
+++ viewContacts.php
@@ -7,13 +7,6 @@ $xml=simplexml_load_file("contacts.xml") or die("Error: Cannot create object");
 //print each child element of contacts
 foreach($xml->children() as $contacts) { 
   echo $contacts->name . "<br>";
-  echo $contacts->address->street . ", ";
-  echo $contacts->address->town . ", ";
-  echo $contacts->address->city . ", ";
-  echo $contacts->address->country . "<br>";
-  echo $contacts->telephoneNum->mobileNum . "<br>";
-  echo $contacts->telephoneNum->homeNum . "<br>";
-  echo $contacts->telephoneNum->workNum . "<br>";
   echo $contacts->email . "<br>";
   echo $contacts->company . "<br>";
   echo $contacts->companyRole . "<br> <br>";
