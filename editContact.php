8c3742f21db894e3d4f9de7feb435acae79f0a22
stuartcarroll91 <stuartcarroll91@gmail.com>
2016-11-11 09:52:16 -0500

    Stu

--- editContact.php
+++ editContact.php
@@ -5,19 +5,45 @@ if(isset($_POST['rename'])){
   
   $name = $_POST['oldName'];
   $newName = $_POST['newName'];
+  $newEmail = $_POST['newEmail'];
+  $newPhoneNum = $_POST['newPhoneNum'];
+  $newCompany = $_POST['newCompany'];
+  $newCompanyRole = $_POST['newCompanyRole'];
   
   $xml = simplexml_load_file("contacts.xml");
-  //$xml->contact[0]->name = "Jamesy";
   
 
   
+  //search contacts for contact with this name
   foreach($xml->xpath("/contacts/contact[name = '$name']") as $node){
-    $node->name = $newName;
+    //change to whatever new name user enters
+    
+    
+    if(!empty($newName)){
+      $node->name = $newName;
+    }
+    
+    if(!empty($newEmail)){
+      $node->email = $newEmail;
+    }
+    
+    if(!empty($newPhoneNum)){
+      $node->phoneNum = $newPhoneNum;l;
+    }
+    
+    if(!empty($newCompany)){
+      $node->company = $newCompany;
+    }
+    
+    if(!empty($newCompanyRole)){
+      $node->companyRole = $newCompanyRole;
+    }
+    
     
   }
   
   
-  //write data to contacts.xml
+  //save data to contacts.xml
   file_put_contents("contacts.xml", $xml->asXML());
   
   
@@ -38,11 +64,24 @@ if(isset($_POST['rename'])){
     Contact Name:<br>
     <input type="text" name="oldName">
     <br>
+    <br>
     New Name:<br>
     <input type="text" name="newName">
     <br>
+    New Email:<br>
+    <input type="text" name="newEmail">
     <br>
-    <input type="submit" name="rename" value="Rename">
+    New Phone Number:<br>
+    <input type="text" name="newPhoneNum">
+    <br>
+    New Company:<br>
+    <input type="text" name="newCompany">
+    <br>
+    New Company Role:<br>
+    <input type="text" name="newCompanyRole">
+    <br>
+    <br>
+    <input type="submit" name="rename" value="Save Changes">
   </form>
   </body>
 </html>
