c684b93db981bcffb1699ecc991bfd77730b71a4
stuartcarroll91 <stuartcarroll91@gmail.com>
2016-12-11 14:12:10 -0500

    s

--- index.php
+++ index.php
@@ -2,56 +2,63 @@
 session_start();
 ?>
 
-<!DOCTYPE html>
-<html>
+	<!DOCTYPE html>
+	<html>
 
-<!-- latest -->
-<head>
-	<title>My Business Contacts </title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="style.css">
-	
-	<link rel="icon" type="image/gif" href="Images/bookicon.ico">
-	
-</head>
+	<!-- latest -->
 
-<body>
+	<head>
+		<title>My Business Contacts </title>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="style.css">
+
+		<link rel="icon" type="image/gif" href="Images/bookicon.ico">
+
+	</head>
+
+	<body>
 
 
 
-	<nav class="navbar navbar-default custom-nav">
-		<div class="container" id="top">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		<nav class="navbar navbar-default custom-nav">
+			<div class="container" id="top">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span> 
       </button>
-				<a class="navbar-brand" href="index.php">MY BUSINESS CONTACTS</a>
-			</div>
-			<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav navbar-right">
-					
+					<a class="navbar-brand" href="index.php">MY BUSINESS CONTACTS</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav navbar-right">
 
 
-        	<?php
+
+						<?php
         	if(isset($_SESSION['id'])){
         		echo "
         		<li id='usernavb'><a href='#'><b>Welcome  ".$_SESSION['first'].   "</b></a></li>
         	<li id='logoutlink'><a href='logout.php'><b>LOG OUT</b></a></li> ";
         	}
         	?>
-        
-				</ul>
-				
+
+					</ul>
+
+				</div>
 			</div>
-		</div>
-	</nav>
+		</nav>
 
 
-	
 
-			<?php
+
+		<?php
 
 
 
@@ -257,44 +264,44 @@ echo
 
   ?>
 
-        <hr>  
+			<hr>
 
 
 
 
-	<footer class="container-fluid">
-		<div class="navbar-fixed-bottom">
+			<footer class="container-fluid">
+				<div class="navbar-fixed-bottom">
 
-			<a href="#top" title="To Top">
-				<span class="glyphicon glyphicon-chevron-up"></span>
-			</a>
+					<a href="#top" title="To Top">
+						<span class="glyphicon glyphicon-chevron-up"></span>
+					</a>
 
-			<p class="copy">
-				<center>This web application is for demonstrational purposes only</center>
-			</p>
+					<p class="copy">
+						<center>This web application is for demonstrational purposes only</center>
+					</p>
 
-		</div>
-	</footer>
-
-
+				</div>
+			</footer>
 
 
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
-	</script>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script src="contactFunctions.js"></script>
-	
-	<!--Smooth Scrolling JQuery*/
+
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+			</script>
+
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+			<script src="contactFunctions.js"></script>
+
+			<!--Smooth Scrolling JQuery*/
 				MUST KEEP LINK BELOW IN FOR SMOOTH SCROLLING...
 
 -->
-	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-	<script src="scrolling.js"></script>
+			<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+			<script src="scrolling.js"></script>
 
 
 
 
-</body>
+	</body>
 
-</html>
+	</html>
